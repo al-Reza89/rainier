653ff1f7f8614bb91970181a69c50a010173e1a1
al-Reza89 <alialreza98@gmail.com>
2023-12-20 02:42:16 +0600

    Add: connected

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" }).status(200);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
